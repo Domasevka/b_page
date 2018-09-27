dcc976e7e90bd0190a98d0e1d99ba3c2b67790fe
domasevka <mdomasevich@gmail.ru>
2018-09-27 10:27:49 +0300

    desktop menu

--- src/js/index.js
+++ src/js/index.js
@@ -1,11 +1,77 @@
 
-<!-- Initialize Swiper -->
 
-  var swiper = new Swiper('.swiper-container', {
+(function() {
+
+  'use strict';
+
+  // breakpoint where swiper will be destroyed
+  // and switches to a dual-column layout
+  var breakpoint = window.matchMedia( '(max-width:868px)' );
+
+  // keep track of swiper instances to destroy later
+  var mySwiper;
+
+  //////////////////////////////////////////////////////////////////
+  //////////////////////////////////////////////////////////////////
+  //////////////////////////////////////////////////////////////////
+
+  var breakpointChecker = function() {
+
+    // if larger viewport and multi-row layout needed
+    if ( breakpoint.matches === true ) {
+
+      // clean up old instances and inline styles when available
+      if ( mySwiper !== undefined ) mySwiper.destroy( true, true );
+
+      // or/and do nothing
+      return;
+
+      // else if a small viewport and single column layout needed
+    } else if ( breakpoint.matches === false ) {
+
+      // fire small viewport version of swiper
+      return enableSwiper();
+
+    }
+
+  };
+
+  //////////////////////////////////////////////////////////////////
+  //////////////////////////////////////////////////////////////////
+  //////////////////////////////////////////////////////////////////
+
+  // var enableSwiper = function() {
+  //
+  //   mySwiper = new Swiper ('.swiper-container', {
+  //
+  //     loop: true,
+  //
+  //     slidesPerView: 'auto',
+  //
+  //     centeredSlides: true,
+  //
+  //     a11y: true,
+  //     keyboardControl: true,
+  //     grabCursor: true,
+  //
+  //     // pagination
+  //     pagination: '.swiper-pagination',
+  //     paginationClickable: true,
+  //
+  //   });
+  //
+  // };
+
+
+  <!-- Initialize Swiper -->
+  var enableSwiper = function() {
+    mySwiper = new Swiper('.swiper-container', {
     slidesPerView: 'auto',
     //slidesPerView: 3,
     centeredSlides: true,
-    spaceBetween: 20,
+    //centeredSlides: false,
+    //spaceBetween: 10,
+
     pagination: {
       el: '.swiper-pagination',
       type: 'fraction',
@@ -14,57 +80,77 @@
       nextEl: '.swiper-button-next',
       prevEl: '.swiper-button-prev',
     },
-    // virtual: {
-    //   slides: (function () {
-    //     var slides = [];
-    //     for (var i = 0; i < 3; i += 1) {
-    //       slides.push('Slide ' + (i + 1));
-    //     }
-    //     return slides;
-    //   }()),
-    // },
-      breakpoints: {
-        1024: {
-          centeredSlides: false,
-        },
-        // 768: {
-        //   centeredSlides: false,
-        // },
-        // 640: {
-        //   slidesPerView: 2,
-        //   spaceBetween: 20,
-        //  },
-        // 320: {
-        //   slidesPerView: 1,
-        //   spaceBetween: 10,
-        // }
-         }
+
+    breakpoints: {
+      868: {
+        centeredSlides: false,
+
+      },
+
+    }
   });
+
+
+  var container = document.getElementById('container');
+
   document.querySelector('.slide-1').addEventListener('mouseover', function (e) {
     e.preventDefault();
-    // var container = document.getElementById('container');
-    //
-    // if (container) {
-    //   container.classList.remove('show');
-    // }
-    // e.currentTarget.classList.add('show');
-    // container.classList.add("show");
-    swiper.slideTo(0, 0);
+    container.classList.add('show');
+    mySwiper.slideTo(0, 0);
   });
+
   document.querySelector('.slide-2').addEventListener('mouseover', function (e) {
     e.preventDefault();
-    swiper.slideTo(1, 0);
+    container.classList.add('show');
+    mySwiper.slideTo(1, 0);
   });
+
   document.querySelector('.slide-3').addEventListener('mouseover', function (e) {
     e.preventDefault();
-    swiper.slideTo(2, 0);
+    container.classList.add('show');
+    mySwiper.slideTo(2, 0);
   });
-// document.querySelector('.slide-1').addEventListener('mouseenter', function(){
-//     console.log('stop autoplay');
-//
-//   });
-// document.querySelector('.slide-1').addEventListener('mouseover', function(){
-//     console.log('start autoplay');
-//
-//   });
-//container.classList.add("show");
+
+  document.getElementById('container').addEventListener('mouseover',   mouseOver);
+  function  mouseOver() {
+    container.classList.add('show');
+  }
+
+  document.getElementById('container').addEventListener('mouseout',  mouseOut);
+  function mouseOut() {
+    container.classList.remove('show');
+  }
+
+  document.querySelector('.js-slide').addEventListener('mouseover',   mouseOverBtn);
+  function  mouseOverBtn() {
+    container.classList.remove('show');
+  }
+  };
+  //////////////////////////////////////////////////////////////////
+  //////////////////////////////////////////////////////////////////
+  //////////////////////////////////////////////////////////////////
+
+  // keep an eye on viewport size changes
+  breakpoint.addListener(breakpointChecker);
+
+  // kickstart
+  breakpointChecker();
+
+
+
+})(); /* IIFE end */
+
+document.body.addEventListener('click', _onClick);
+
+function _onClick(evt) {
+  //снятие класса clicked с активного элемента
+  var сlickedElement = document.querySelector('.js-menu-btn');
+  var container = document.getElementById('container');
+
+  if (сlickedElement) {
+    container.classList.add('show');
+  }
+  //добавление класса сlicked элементу, на котором
+  //сработал обработчик события
+  //evt.currentTarget.classList.add('сlicked');
+}
